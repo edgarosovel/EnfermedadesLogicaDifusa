846fa7cd33a1daf5f473560fd1db2d98203fc37c
edgarosovel <edgarov@live.com.mx>
2018-03-13 19:14:50 -0600

    v1

--- enfermedades.js
+++ enfermedades.js
@@ -3,20 +3,19 @@ var enfermedades = [
 		"nombre":"Bronquiectasia",
 		"descripcion":"Es una enfermedad inflamatoria crónica  de las vías aéreas que implica la dilatación de los bronquios.",
 		"recomendacion":"La principal vía de tratamiento consiste en mantener libres las vías respiratorias y evitar posibles obstrucciones y complicaciones derivadas de las bronquiectasias",
-		"sumatoria":8,
 		"sintomas":{
 			"Piel con color azul":0.6,
 			"Halitosis o mal aliento bucal":0.5,
 			"Tos crónica con esputo fétido (flema originada en el pulmón con mal olor)":0.6,
 			"Infecciones bronquiales repetidas":0.7,
 			"Dedos de la mano hinchados en la parte más distal":0.7,
-			"Expectoración con sangre originada en los bronquios o los pulmones":0.6,
+			"Expectoración con sangre originada en los bronquios o los pulmones":0.7,
 			"Fatiga":0.5,
 			"Fiebre":0.5,
 			"La tos suele producir expectoración con pus":0.5,
 			"Dolor torácico":0.5,
 			"Palidez":0.4,
-			"Problemas respiratorios,en especial cuando se hace deporte":0.7,
+			"Problemas respiratorios,en especial cuando se hace deporte":0.6,
 			"Perdia de peso":0.5,
 			"Sibilancias o sonido que hace el aire que se genera en las vías respiratorias cuando están obstruidas o congestionadas":0.3,
 			"Dolor en el Pecho":0.4
@@ -26,7 +25,6 @@ var enfermedades = [
 		"nombre":"Edema Pulmonar",
 		"descripcion":"Un edema pulmonar es una acumulación anormal de líquido en los pulmones que provoca dificultad respiratoria.",
 		"recomendacion":"Seguir una dieta saludable, baja en grasas y sal también disminuye considerablemente el riesgo de padecer esta enfermedad.",
-		"sumatoria":8,
 		"sintomas":{
 			"Expectoración de sangre o espuma con sangre":0.6,
 			"Incapacidad para pronunciar oraciones largas completas debido a la falta de aire":0.5,
@@ -43,7 +41,6 @@ var enfermedades = [
 			"Aumento de la frecuencia cardíaca":0.5,
 			"Disminución de la lucidez menta":0.6,
 			"Despertarse a la noche con sensación de dificultad para respirar que puede aliviarse al sentarse":0.6
-
 			}
 	},
 
@@ -51,13 +48,12 @@ var enfermedades = [
 		"nombre":"Epoc",
 		"descripcion":"La enfermedad pulmonar obstructiva crónica, conocida con las siglas EPOC, consiste en la obstrucción persistente de las vías respiratorias",
 		"recomendacion":"Alimentación saludable, hacer ejercisio y no fumar",
-		"sumatoria":8,
 		"sintomas":{
 			"Tos y aumento de la mucosidad, normalmente al levantarse por la mañana":0.6,
 			"Tendencia a sufrir resfriados de pecho":0.5,
 			"El esputo que se produce durante estos resfriados con frecuencia se vuelve amarillo o verde debido a la presencia de pus":0.5,
 			"Infecciones respiratorias frecuentes":0.4,
-			"Respiración sibilante.":0.7,
+			"Respiración sibilante":0.7,
 			"Sensación de ahogo cuando se hace un esfuerzo y, más adelante, ahogo en actividades diarias, como lavarse, vestirse y preparar la comida":0.6,
 			"Fatiga":0.5,
 			"Fiebre":0.5,
@@ -68,14 +64,12 @@ var enfermedades = [
 			"Aumento de la frecuencia cardíaca":0.5,
 			"Presión en el pecho":0.6,
 			"Falta de energía":0.6
-
 			}
 	},
 	{
 		"nombre":"Neumonía",
 		"descripcion":"Es una infección del pulmón caracterizada por la multiplicación de microorganismos en el interior de los alvéolos, lo que provoca una inflamación con daño pulmonar.",
 		"recomendacion":"La abstención de fumar, reducir el consumo de alcohol y mejorar los hábitos higiénicos de limpieza oral y dental.",
-		"sumatoria":8,
 		"sintomas":{
 			"Dificultad para respirar":0.3,
 			"Resfriado":0.5,
@@ -92,14 +86,12 @@ var enfermedades = [
 			"Vómitos ":0.5,
 			"Labios azules ":0.6,
 			"Náuseas":0.6
-
 			}
 	},
 	{
 		"nombre":"Asma",
 		"descripcion":"La enfermedad aparece como respuesta a determinados estímulos que producen alergia: polen, ácaros del polvo, partículas de la piel de gato y perro, humo, aire frío, ciertos alimentos o aditivos alimenticios.",
 		"recomendacion":"evitar realizar ejercicios intensos en época de polinización, ya que esto puede generar dificultades respiratorias que deriven en un ataque de asma.",
-		"sumatoria":8,
 		"sintomas":{
 			"Dificultad para respirar":0.4,
 			"Resfriado":0.3,
@@ -116,20 +108,18 @@ var enfermedades = [
 			"Tos":0.5,
 			"Está confundido o reacciona menos de lo habitual":0.6,
 			"Opresión en el pecho":0.7
-
 			}
 	},
 	{
 		"nombre":"Silicosis",
 		"descripcion":"La silicosis es una enfermedad que no tiene cura y que afecta al sistema respiratorio por inhalar polvo sílice",
 		"recomendacion":"Lave sus manos y cara antes de comer, beber, ir al baño, fumar o ponerse maquillaje.",
-		"sumatoria":8,
 		"sintomas":{
 			"Dificultad para respirar":0.5,
 			"Debilidad general":0.3,
 			"Sudores nocturnos":0.5,
 			"Problemas respiratorios,en especial cuando se hace deporte":0.6,
-			"Dolores en el pecho.":0.7,
+			"Dolores en el pecho":0.7,
 			"Mal aliento severo":0.4,
 			"Fatiga":0.3,
 			"Fiebre":0.5,
@@ -140,14 +130,12 @@ var enfermedades = [
 			"Tos fuerte":0.5,
 			"Insuficiencia respiratoria":0.7,
 			"Perdida del apetito.":0.5
-
 			}
 	},
 		{
 		"nombre":"Enfisema",
 		"descripcion":"El enfisema es un tipo de enfermedad pulmonar obstructiva crónica (EPOC) por la que se produce la destrucción del tejido pulmonar, principalmente por fumar.",
 		"recomendacion":"No exponerse a los factores de riesgo que la originan: el tabaco y los ambientes contaminados.",
-		"sumatoria":8,
 		"sintomas":{
 			"Dificultad para respirar":0.6,
 			"Debilidad general":0.3,
@@ -160,18 +148,16 @@ var enfermedades = [
 			"Trabajo para recobrar el aliento o hablar":0.7,
 			"uñas de color azul":0.5,
 			"Tonalidad de Piel de color azul":0.5,
-			"Dificultad al respirar cuando hace esfuerzo físico.":0.5,
+			"Dificultad al respirar cuando hace esfuerzo físico":0.5,
 			"Está confundido o reacciona menos de lo habitual":0.5,
 			"Insuficiencia respiratoria":0.7,
 			"El corazón le tarde muy rápido":0.6
-
 			}
 	},
 	{
 		"nombre":"Cáncer de pulmón",
 		"descripcion":"Se produce cuando hay un crecimiento exagerado de células malignas en este órgano.",
 		"recomendacion":" Si acude al médico cuando comienza a notar los síntomas, es posible que el cáncer se diagnostique en una etapa temprana cuando es más probable que el tratamiento sea más eficaz.",
-		"sumatoria":8,
 		"sintomas":{
 			"Tos con sangre o esputo (saliva o flema) del color del metal oxidado":0.7,
 			"Dolor en el pecho que a menudo empeora cuando respira profundamente, tose o se ríe":0.5,
@@ -188,14 +174,12 @@ var enfermedades = [
 			"Infecciones como bronquitis y neumonía que no desaparecen o que siguen recurriendo":0.5,
 			"Parición de sibilancias (silbido de pecho)":0.7,
 			"Dolor en los huesos":0.5
-
 			}
 	},
 	{
 		"nombre":"Faringitis",
 		"descripcion":"Se produce un hinchazón, normalmente de la parte posterior de la garganta; en la mayoría de los casos como consecuencia de resfriados, gripe u otros virus.",
 		"recomendacion":"Es recomendable descansar la voz o beber mucha agua para mantener la humedad de la garganta y prevenir la deshidratación.",
-		"sumatoria":8,
 		"sintomas":{
 			"Dolor de garganta":0.7,
 			"Sarampión":0.5,
@@ -212,18 +196,16 @@ var enfermedades = [
 			"Dolor que empeora al tragar o hablar":0.7,
 			"Ganglios inflamados en el cuello":0.7,
 			"Congestión":0.4
-
 			}
 	},
 	{
 		"nombre":"Gripe",
 		"descripcion":"Se produce como resultado de virus que, generalmente, produce infección de garganta, pulmones y nariz.",
 		"recomendacion":"Es aconsejable prevenir la gripe no solo con la vacunación, si no evitando el contagio, lavándose las manos cuando se cree ha estado en contacto con el virus",
-		"sumatoria":8,
 		"sintomas":{
 			"Cuerpo Cortado":0.7,
 			"Escalofríos":0.5,
-			"Pérdida de peso ":0.3,
+			"Pérdida de peso":0.3,
 			"Mareos":0.5,
 			"Dolor de cabeza":0.7,
 			"nauseas":0.5,
@@ -236,7 +218,6 @@ var enfermedades = [
 			"Respiración acelerada":0.4,
 			"Fatiga (cansancio)":0.7,
 			"Diarrea":0.4
-
 			}
 	}
 ]
--- main.js
+++ main.js
@@ -1,50 +1,203 @@
+const UMBRAL = 3; //La máxima suma de síntomas de una enfermedad es 8
+
+function cambiarVista(id_vista_actual, id_vista_nueva){
+    // OCULTAMOS VISTA ACTUAL
+    vista_actual = document.getElementById(id_vista_actual);
+    vista_actual.classList.toggle(id_vista_actual);
+    vista_actual.classList.toggle(id_vista_actual+"-oculto");
+    // MOSTRAMOS VISTA NUEVA
+    vista_nueva = document.getElementById(id_vista_nueva);
+    vista_nueva.classList.toggle(id_vista_nueva);
+    vista_nueva.classList.toggle(id_vista_nueva+"-oculto");
+}
+
 function actualizarValorSlider(slider){
-    document.getElementById("val"+slider.id).innerText = "Valor: "+ (Number(slider.value / 100));
+    document.getElementById("val"+slider.id).innerText = "Valor: "+ (Number(slider.value / 10));
+    slider.style = "background-size: "+slider.value+"% 2px"
 } 
 
 function start(){
-    contenedor = document.getElementById('contenedor-sintomas');
+    contenedor_sintomas = document.getElementById('contenedor-sintomas');
+    contenedor_sintomas2 = document.getElementById('contenedor-sintomas2');
+    html = '';
+    html2 = '';
     for (var sintoma of sintomas){
-        celda = '<div class="sintomas"><h2 class="nombre-sintoma">'+sintoma.nombre+'</h2><span class="notification" id="val'+sintoma.nombre+'">Valor: 0</span><br>';
-        celda += '<div class="range"><input type="range" min="0" max="100" value="0" class="range__input" onchange="actualizarValorSlider(this)" id="'+sintoma.nombre+'"></div>';
-        celda += '<p class="izquierda">'+ sintoma.escala[0] + '</p><p class="derecha">' + sintoma.escala[1] + '</p></div>';
-        contenedor.innerHTML += celda;
+        html += '<div class="sintomas"><h2 class="nombre-sintoma">'+sintoma.nombre+'</h2><span class="notification" id="val'+sintoma.nombre+'">Valor: 0</span><br><div class="range"><input type="range" min="0" max="100" value="0" class="range__input range__input--active range_sintomas" onchange="actualizarValorSlider(this)" oninput="actualizarValorSlider(this)" id="'+sintoma.nombre+'"></div><p class="izquierda">'+ sintoma.escala[0] + '</p><p class="derecha">' + sintoma.escala[1] + '</p></div>';   
+        html2 += '<div class="sintomas"><h2 class="nombre-sintoma">'+sintoma.nombre+'</h2><span class="notification" id="val2'+sintoma.nombre+'">Valor: 0</span><br><div class="range"><input type="range" min="0" max="100" value="0" class="range__input range__input--active range_sintomas2" onchange="actualizarValorSlider(this)" oninput="actualizarValorSlider(this)" id="2'+sintoma.nombre+'"></div><p class="izquierda">'+ sintoma.escala[0] + '</p><p class="derecha">' + sintoma.escala[1] + '</p></div>';   
     }
+    contenedor_sintomas.innerHTML = html;
+    contenedor_sintomas2.innerHTML = html2;
+    contenedor_enfermedades = document.getElementById('contenedor-enfermedades');
+    html = '';
+    for (var enfermedad of enfermedades){
+        html+= '<li class="list-item list-item--tappable">'+
+        '<div class="list-item__left"><label class="checkbox">' +
+            '<input type="checkbox" id="'+enfermedad.nombre+'" class="checkbox__input" name="enfermedades">' +
+            '<div class="checkbox__checkmark"></div></label></div>' +
+        '<label for="'+enfermedad.nombre+'" class="list-item__center">'+enfermedad.nombre+'</label></li>';
+    }
+    contenedor_enfermedades.innerHTML = html;
 }
 
-function empezar(){
-    document.getElementById('inicio').classList.add('inicio-oculto');
+function mostrar_diagnostico_general(){
+    cambiarVista("inicio","diagnostico-general");
 }
 
-function evaluar(){
-	inputs = document.getElementsByClassName('range__input');
-	valores = Array();
-    datos = Array();
+function mostrar_diagnostico_especifico(){
+    cambiarVista("inicio","diagnostico-especifico");
+}
+
+function general_a_principal(){
+    cambiarVista("diagnostico-general","inicio");
+}
+
+function especifico_a_principal(){
+    cambiarVista("diagnostico-especifico","inicio");
+}
+
+function mostrar_resultados(resultados, enfermedades=undefined){
+    res = document.getElementById("resultados");
+    res.classList.toggle("resultados-oculto");
+    res.classList.toggle("resultados");
+
+    div_resultados = document.getElementById("info-resultado");
+    msj = "";
+    if (enfermedades){
+        msj+="<p><b>Enfermedades seleccionadas:</b><p>";
+        enfermedades.forEach(function(enfermedad){
+            msj+="<p>"+enfermedad.nombre+"</p>";
+        });
+        msj+="<br>";
+    }
+    if (resultados.length == 1){
+        msj+="<p>Esta es la enfermedad que mejor cuadra con tus síntomas:</p><br>";
+        msj+="<h3>"+resultados[0].enfermedad.nombre+"</h3>";
+        msj+="<p>"+resultados[0].enfermedad.descripcion+"</p><br>";
+        msj+="<p><b>Recomendaciones:</b></p><p>"+resultados[0].enfermedad.recomendacion+"</p>";
+    }else if(resultados.length>1){
+        msj+="<p>Las siguientes enfermedades cuadran con tus síntomas:</p><br>";
+        resultados.forEach(function(resultado) {
+            msj+="<h3>"+resultado.enfermedad.nombre+"</h3>";
+            msj+="<p>"+resultado.enfermedad.descripcion+"</p><br>";
+            msj+="<p><b>Recomendaciones:</b></p><p>"+resultado.enfermedad.recomendacion+"</p><br><br>";
+        });
+    }else{
+        msj="Ninguna enfermedad cuadra con sus síntomas.";
+    }
+    div_resultados.innerHTML = msj;
+}
+
+function ocultar_resultados(){
+    res = document.getElementById("resultados");
+    res.classList.toggle("resultados-oculto");
+    res.classList.toggle("resultados");
+}
+
+function evaluar_general(){
+	inputs = document.getElementsByClassName('range_sintomas');
+	sintomas_paciente = Array();
+    resultados = Array();
 
     //Se obtienen los valores de los síntomas del usuario
-	for (var input of inputs) {
-		valores.push(
+    [...inputs].forEach(function(input) {
+        sintomas_paciente.push(
             { 
                 "nombre" : input.id, 
-                "valor" : Number(input.value/10)
+                "valor" : Number(input.value / 100)
             }
         );
-    }
+    });
     
-    for (var x =0; x < enfermedades.length; x++){
-        suma = 0;
-        for (var z=0; z< valores.length; z++){
-            indice = valores[z].nombre;
-            suma += (enfermedades[x].sintomas[indice] != undefined)? Math.min(enfermedades[x].sintomas[indice],valores[z].valor) : 0;
-        }	
-        ref = Number(enfermedades[x].sumatoria);
-
-        if ( suma > (ref - 1) && suma < (ref + 1) ){
-            element = {'nombre': enfermedades[x].nombre, 'descripcion': enfermedades[x].descripcion, 'recomendacion': enfermedades[x].recomendacion};
-            datos.push(element);	
+    enfermedades.forEach(function(enfermedad) {
+        suma_de_membresia = 0;
+        sintomas_paciente.forEach(function(sintoma){
+            suma_de_membresia += Math.min(enfermedad.sintomas[sintoma.nombre], sintoma.valor) || 0;
+        });
+        if (suma_de_membresia>UMBRAL){
+            if (resultados[0] && resultados[0].membresia < suma_de_membresia){
+                resultados = [
+                    {
+                        membresia: suma_de_membresia,
+                        enfermedad: enfermedad
+                    }
+                ];
+            }else if((resultados[0] && resultados[0].membresia == suma_de_membresia) || !resultados[0]){
+                resultados.push(
+                    {
+                        membresia: suma_de_membresia,
+                        enfermedad: enfermedad
+                    }
+                );
+            }
         }
-    }
-    datos = (datos.length > 0) ? datos:null;
+    });
+    mostrar_resultados(resultados);
+}
 
-    alert(datos[1].nombre);
+function evaluar_especifico(){
+    inputs = document.getElementsByClassName('range_sintomas2');
+    enfermedades_inputs = document.getElementsByClassName('checkbox__input');
+    enfermedades_seleccionadas = Array();
+    [...enfermedades_inputs].forEach(function(input) {
+        if (input.checked){
+            for (var enfermedad of enfermedades){
+                if (enfermedad.nombre==input.id){
+                    enfermedades_seleccionadas.push(enfermedad);
+                    break;
+                }
+            }
+        }
+    });
+    if(enfermedades_seleccionadas.length<2){
+        toast("Seleccione dos o más enfermedades de la lista para realizar un diagnóstico");
+        return
+    }
+	sintomas_paciente = Array();
+    resultados = Array();
+
+    //Se obtienen los valores de los síntomas del usuario
+    [...inputs].forEach(function(input) {
+        sintomas_paciente.push(
+            { 
+                "nombre" : input.id, 
+                "valor" : Number(input.value / 100)
+            }
+        );
+    });
+    
+    enfermedades_seleccionadas.forEach(function(enfermedad) {
+        suma_de_membresia = 0;
+        sintomas_paciente.forEach(function(sintoma){
+            suma_de_membresia += Math.min(enfermedad.sintomas[sintoma.nombre], sintoma.valor) || 0;
+        });
+        if (suma_de_membresia>UMBRAL){
+            if (resultados[0] && resultados[0].membresia < suma_de_membresia){
+                resultados = [
+                    {
+                        membresia: suma_de_membresia,
+                        enfermedad: enfermedad
+                    }
+                ];
+            }else if((resultados[0] && resultados[0].membresia == suma_de_membresia) || !resultados[0]){
+                resultados.push(
+                    {
+                        membresia: suma_de_membresia,
+                        enfermedad: enfermedad
+                    }
+                );
+            }
+        }
+    });
+    mostrar_resultados(resultados,enfermedades_seleccionadas);
+}
+
+function toast(mensaje){
+    toast_element = document.getElementById("toast");
+    toast_element.classList.toggle("toast");
+    // toast_element.classList.toggle("toast-oculto");
+    toast_element.children[0].innerHTML= "<b>ADVERTENCIA: </b>"+mensaje;
+    setTimeout(function(){
+        toast_element.classList.toggle("toast");
+        // toast_element.classList.toggle("toast-oculto");
+    },3500);
 }
